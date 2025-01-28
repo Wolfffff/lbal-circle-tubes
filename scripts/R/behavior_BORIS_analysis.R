7f652d8cd90ad4efb7db9a1ee2db597710be7474
michellewhite04 <mw0425@princeton.edu>
2025-01-27 19:16:41 -0500

    uploading duration analysis + individual interaction analysis

--- scripts/R/behavior_BORIS_analysis.R
+++ scripts/R/behavior_BORIS_analysis.R
@@ -19,10 +19,24 @@ video_list <- video_data$videoname
 contrast_list <- gsub(video_data$contrast, pattern = "-", replacement = "_")
 nest_site_ids <- video_data$nest_ID_contrast
 
+# Extract counts for each behavioral category
 behavior_data_grouped <- behavior_data %>%
   group_by(videoname, Behavioral.category) %>%
+  filter(Behavior.type == "START") %>% # Only consider START times for counting
   summarise(count = n(), .groups = "drop") %>%
   complete(videoname, Behavioral.category, fill = list(count = 0))
+
+# Extract interaction duration using START and STOP times
+behavior_data_grouped <- behavior_data %>%
+  arrange(videoname, Subject, Behavioral.category, Behavior, Time) %>%
+  mutate(
+    next_time = lead(Time),
+    Duration = next_time - Time
+  ) %>%
+  filter(Behavior.type == "START") %>%
+  group_by(videoname, Behavioral.category) %>%
+  summarise(avg_duration = mean(Duration), .groups = "drop") %>%
+  complete(videoname, Behavioral.category, fill = list(avg_duration = 0))
 
 aggressive_list <- behavior_data_grouped %>% filter(Behavioral.category == "Aggressive") %>% pull(count)
 avoidant_list <- behavior_data_grouped %>% filter(Behavioral.category == "Avoidant") %>% pull(count)
@@ -92,7 +106,7 @@ for (measure in measures) {
   box_plots <- ggplot(lmm_df, aes(x = contrast, y = y_values, color = contrast)) +
     geom_boxplot(alpha = 0.5, outlier.shape = NA, width = 0.6) +
     geom_jitter(aes(color = contrast), width = 0.15, size = 2, alpha = 0.8) +
-    geom_text(data = emm_df, aes(x = contrast, y = c(60, 60, 60, 60, 60), label = Letters), vjust = -0.5, hjust = -0.3, size = 5, color = "black") +
+    geom_text(data = emm_df, aes(x = contrast, y = rep(30, 5), label = Letters), vjust = -0.5, hjust = -0.3, size = 5, color = "black") +
     scale_color_manual(values = CONTRAST_COLORS) +
     scale_x_discrete(labels = c("queen_solitary" = "Q-S", "queen_queen" = "Q-Q",
                                 "solitary_solitary" = "S-S", "queen_worker" = "Q-W",
@@ -112,3 +126,81 @@ for (measure in measures) {
 # Save the plot to the specified directory
 whole_plot <- (plotlist[[1]] | plotlist[[2]]) / (plotlist[[3]] | plotlist[[4]])
 ggsave("../../figures/emm_interaction_counts.jpeg", whole_plot, width = 8, height = 6, dpi = 300)
+
+### Individual Analysis Here ###*****************************************************************************************************
+# Extract interactions counts for individuals
+behavior_data_grouped <- behavior_data %>%
+  filter(Behavior.type == "START") %>%
+  mutate(Tag_Color = case_when(
+    Subject == "Black Tag" ~ "B",
+    Subject == "Purple Tag" ~ "P",
+    TRUE ~ NA_character_
+  )) %>%
+  filter(!is.na(Tag_Color)) %>%
+  left_join(master_data, by = c("videoname" = "Videoname", "Tag_Color" = "Tag.Color")) %>%
+  select(Subject, Behavior, Behavioral.category, Behavior.type, Time, Image.index, videoname, Specimen.ID, Caste) %>%
+  group_by(Specimen.ID, Behavioral.category, Caste) %>%
+  summarise(count = n(), .groups = "drop") %>%
+  complete(Specimen.ID, Behavioral.category, Caste, fill = list(count = 0))
+
+behavioral_categories <- unique(behavior_data_grouped$Behavioral.category)
+
+# Create a list to store the plots and initialize a counter
+plotlist <- list()
+i <- 1
+
+for (category in behavioral_categories) {
+  # Prepare the data: filter missing values, select relevant columns, and factorize the "Caste" variable
+  plot_df <- behavior_data_grouped %>%
+    filter(Behavioral.category == category) %>%
+    mutate(Caste = factor(Caste, levels = c("queen", "worker", "solitary")))
+
+  # Conduct pairwise t-tests between groups and adjust p-values using the Bonferroni method
+  stats_df <- plot_df %>%
+    pairwise_t_test(formula = count ~ Caste, p.adjust.method = "bonferroni")
+
+  # Filter out non-significant comparisons and select only significant group pairs
+  significant_comparisons <- stats_df %>%
+    filter(p.adj <= 0.05) %>%
+    select(group1, group2)
+
+  # Convert the filtered comparisons into a list format suitable for stat_compare_means
+  comparisons_list <- if (nrow(significant_comparisons > 0)) {
+    lapply(
+      split(significant_comparisons, seq.int(nrow(significant_comparisons))),
+      function(x) as.character(unlist(x))
+    )
+  } else { 
+    list()
+  }
+
+  # Create a plot for ovarian development per caste with significance annotations
+  box_plots <- ggplot(plot_df, aes(x = Caste, y = count, fill = Caste)) +
+    geom_boxplot(alpha = 0.5, outlier.shape = NA, width = 0.6) +
+    geom_jitter(aes(color = Caste), width = 0.15, size = 2, alpha = 0.8) +
+    scale_fill_manual(values = CASTE_COLORS) +
+    scale_color_manual(values = CASTE_COLORS) +
+    labs(
+      title = paste0(category, " Interactions per Caste"),
+      x = "Caste",
+      y = paste0("# of ", category, "Interactions")
+    ) +
+    stat_compare_means(
+      method = "t.test",
+      label = "p.signif",
+      comparisons = comparisons_list,
+      hide.ns = TRUE,
+      step.increase = 0.1
+    ) +
+    scale_y_continuous(expand = expansion(mult = c(0.05, 0.2))) +  # Add more space above the plot
+    SHARED_THEME
+
+  # Add the plot to the list of plots and increment the counter
+  plotlist[[i]] <- box_plots
+  i <- i + 1
+
+}
+
+# Save the plot to the specified directory
+whole_plot <- (plotlist[[1]] | plotlist[[2]]) / (plotlist[[3]] | plotlist[[4]])
+ggsave("../../figures/individual_behaviors_signif.jpeg", whole_plot, width = 8, height = 6, dpi = 300)
